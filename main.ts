8fef91b2083bd201717f244f3a1c722e90a1706f
phy0hk <phyoheinko.phk1@gmail.com>
2025-03-25 22:41:30 +0630

    Port added

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import { Application } from "jsr:@oak/oak/application";
 import { Router } from "jsr:@oak/oak/router";
 import { mainPortalRouter } from "./Applications/MainPortal/Router/Main.ts";
-
+import "https://deno.land/std@0.181.0/dotenv/load.ts";
 const app = new Application();
-const port = 3000;
+const port:number = Deno.env.get("PORT") | 3000;
 
 app.use(mainPortalRouter.routes());
 app.use(mainPortalRouter.allowedMethods());
