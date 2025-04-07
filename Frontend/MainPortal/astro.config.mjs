78d410272339e14c3baf58eaebe7ae8ccba29ace
phy0hk <phyoheinko.phk1@gmail.com>
2025-04-07 21:35:29 +0630

    Update

--- Frontend/MainPortal/astro.config.mjs
+++ Frontend/MainPortal/astro.config.mjs
@@ -11,4 +11,8 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
       },
+    // server: {
+    //   // @ts-ignore
+    //   allowedHosts:"joy-ver-oliver-hopkins.trycloudflare.com"
+    // }
 });
